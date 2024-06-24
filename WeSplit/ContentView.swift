7453047e7ab5bfd88d5bcd8dd140becd98bc767e
GarifullinII <ildaridrisovich@gmail.com>
2024-06-24 16:26:39 +0400

    add navigation bar, button

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack{
+            Button("Tap count: \(tapCount)") {
+                tapCount += 1
+            }.padding(10)
+            Form{
+                Section{
+                    Text("Hello, world!")
+                }
+            }
+            .navigationTitle("SwiftUI")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
     }
 }
 
