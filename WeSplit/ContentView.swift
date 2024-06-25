821889486cace7ca2bfae26deb506bac95c43c62
GarifullinII <ildaridrisovich@gmail.com>
2024-06-25 18:24:09 +0400

    add logic

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,32 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
-    let students = ["Anna", "Ivan", "Olga"]
-    @State private var selectedStudent = "Olga"
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amoundIsFocused: Bool
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
     
     var body: some View {
-        NavigationStack{
-            Picker("Select your student", selection: $selectedStudent, content: {
-                ForEach(students, id: \.self) {
-                    Text($0)
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount",
+                              value: $checkAmount,
+                              format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    .keyboardType(.decimalPad)
+                    .focused($amoundIsFocused)
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
                 }
-            })
-            Form{
-                Section{
-                    TextField("Enter your name", text:$selectedStudent)
+                
+                Section("How much do you want to tip?") {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                    .pickerStyle(.segmented)
                 }
-                Section{
-                    Text("Hello, your name is: \(selectedStudent)!")
+                
+                Section {
+                    Text(totalPerPerson,
+                         format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 }
             }
-            .navigationTitle("SwiftUI")
-            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle("WeSplit")
+            .toolbar {
+                if amoundIsFocused {
+                    Button("Done") {
+                        amoundIsFocused = false
+                    }
+                }
+            }
         }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
